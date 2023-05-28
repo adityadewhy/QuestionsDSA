8008a60302ed12424e6e0c6b8dfce40223609d1e
Aditya Sharma <130918775+adityadewhy@users.noreply.github.com>
2023-05-28 19:08:52 +0530

    optimized RichestCustomerWealth.java
    using math.max to compare two int instead of appending every item to array and find max of array

--- RichestCustomerWealth.java
+++ RichestCustomerWealth.java
@@ -11,3 +11,20 @@ class Solution {
         return Arrays.stream(wealth).max().getAsInt();
     }
 }
+
+
+
+#more optimized
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int maxw = 0;
+        for(int person=0 ; person < accounts.length ; person++){
+            int currw = 0;
+            for (int banks=0 ; banks < accounts[person].length ; banks++){
+                currw += accounts[person][banks];
+            }
+            maxw = Math.max(maxw,currw);
+        }
+        return maxw;
+    }
+}
