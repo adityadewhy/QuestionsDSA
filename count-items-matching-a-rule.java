c8c9e822b642caf4a3e20693cdf2a44de5afd3b8
Aditya Sharma <130918775+adityadewhy@users.noreply.github.com>
2023-06-03 04:07:07 +0530

    even more optimization possible

--- count-items-matching-a-rule.java
+++ count-items-matching-a-rule.java
@@ -44,3 +44,29 @@ class Solution {
     }
     
 ###########################################################################################
+
+#2 3ms 47.5MB
+
+class Solution {
+    public int countMatches(List<List<String>> s, String k, String v) {
+        int c=0;
+        int x=0;
+        int n=s.size();
+        String a[]={"type","color","name"};
+        if(k.equals("type")){
+            x=0;
+        }
+        else if(k.equals("color")){
+            x=1;
+        }
+        else if(k.equals("name")){
+            x=2;
+        }
+        for(int i=0;i<n;i++){
+            if(s.get(i).get(x).equals(v)){
+                c++;
+            }
+        }
+        return c;
+    }
+}
