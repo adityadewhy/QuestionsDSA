ae6b6e6a95f3692d8f48738fc58a3012e93112d0
Aditya Sharma <130918775+adityadewhy@users.noreply.github.com>
2023-06-12 21:53:45 +0530

    System.gc()

--- find-numbers-with-even-number-of-digits.java
+++ find-numbers-with-even-number-of-digits.java
@@ -1,7 +1,7 @@
-# https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+// https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
 
-#1 41.8MB 5ms
-# just played with constraints
+// 41.8MB 5ms
+// just played with constraints
 
 class Solution {
     public int findNumbers(int[] nums) {
@@ -23,5 +23,33 @@ class Solution {
             }
         }
         return evens;
+    }
+}
+
+//2 41MB 
+class Solution {
+    public int findNumbers(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            int num=numofdigit(nums[i]);
+            if(num%2==0){
+                count++;
+            }
+        }
+        System.gc();
+        
+//        just adding System.gc() made it grom from 42.9 to 41
+        
+        return count;
+    }
+    int numofdigit(int num){
+        int nod=0;
+        while(num!=0)
+        {
+            num=num/10;
+            nod++;
+        }
+        return nod;
     }
 }
