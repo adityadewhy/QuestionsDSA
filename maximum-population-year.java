4f2292085186823b834e0fd1d810b09341cfa43b
Aditya Sharma <130918775+adityadewhy@users.noreply.github.com>
2023-06-17 17:32:48 +0530

    redo 1 +

---
+++ maximum-population-year.java
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/maximum-population-year/submissions/973303180/
+
+//40.5MB
+class Solution {
+    public int maximumPopulation(int[][] logs) {
+        int[] tally = new int[101];
+        //2050-1950
+
+        for (int i = 0 ; i < logs.length ; i++){
+            for (int j = logs[i][0] ; j < logs[i][1] ; j++){
+                tally[j-1950] += 1;
+            }
+        }
+        System.out.println(Arrays.toString(tally));
+        int maxyear = logs[0][0];
+        int maxpop = 0;
+        for (int i = 0 ; i < 101 ; i++){
+            if(tally[i] > maxpop){
+                maxpop = tally[i];
+                maxyear = i+1950;
+            }
+        }
+        return maxyear;
+        
+    }
+}
