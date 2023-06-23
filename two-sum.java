a9801120b2981ca73100623f97386d14a1a4dd69
Aditya Sharma <130918775+adityadewhy@users.noreply.github.com>
2023-06-23 12:32:26 +0530

    optimize

---
+++ two-sum.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/two-sum/submissions/977653069/
+
+//43.5MB
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        for (int i = 0 ; i < nums.length ; i++){
+            for (int j = 0 ; j < nums.length ; j++){
+                if( i != j && (nums[i]+nums[j]) == target){
+                    ans[0] = i;
+                    ans[1] = j;
+                }
+            }
+        }
+        return ans;
+    }
+}
