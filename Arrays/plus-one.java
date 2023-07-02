86193299507b21a60af8cd5451a0e2d479e8ca5e
Aditya Sharma <130918775+adityadewhy@users.noreply.github.com>
2023-07-03 02:18:01 +0530

    redo 1

--- Arrays/plus-one.java
+++ Arrays/plus-one.java
@@ -1,30 +1,31 @@
-//https://leetcode.com/problems/plus-one/
+// https://leetcode.com/problems/plus-one/submissions/984822769/
 
-/*
 class Solution {
     public int[] plusOne(int[] digits) {
-        StringBuilder sb = new StringBuilder();
-        for (int num : digits){
-            sb.append(num);
-        }
-        String digitsS = sb.toString();
-        int digitsI = Integer.parseInt(digitsS);
 
-        int ansI = digitsI + 1;
+        int last = digits.length - 1;
         
-        String ansS = String.valueOf(ansI);
-        int lenS = ansS.length();
-
-        int[] ans = new int[lenS];
-        for (int i = 0; i < lenS; i++) {
-            char digitChar = ansS.charAt(i); 
-            // Get the character at each position
-            int digit = Character.getNumericValue(digitChar); 
-            // Convert the character to an integer
-            ans[i] = digit; 
-            // Store the digit in the int array
+        for(int i = last ; i >= 0 ; i--){
+            if(digits[i] != 9){
+                digits[i] = digits[i] + 1;
+                break;
+            }else{
+                digits[i] = 0;
+            }
         }
-        return ans;
+
+        //this is for the case when all digits are 9. other cases will be handled by above for loop
+        if(digits[0] == 0){
+            int[] larger = new int[last + 2];
+            larger[0] = 1;
+            // the reason why we dont interfere with any other index is because by default new array have 0 at every index.
+            //what we intended for this edge case was to anyway make say 99->100
+            //we just have to create a bigger array with first index value 1 and rest all can anyway be 0
+
+            return larger;
+        }
+
+        return digits;
+
     }
 }
-*/
